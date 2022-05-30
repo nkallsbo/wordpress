d41981db1f80ae8b15630951a92a8ad245e3f863
Natalie Kallsbo <natalie@kallsbo.se>
2022-05-30 19:14:55 +0200

    Create wpinstall.sh
    - Mounts a folder where all contents for wordpress saves. 
    - Installs wordpress

---
+++ wpinstall.sh
@@ -0,0 +1,14 @@
+# Mount to EFS
+mount -t efs -o tls fs-09a5941278d57ae6f:/ /var/www/html/
+# Download Wordpress
+wget https://wordpress.org/latest.tar.gz
+# Unzip files
+tar -xzf latest.tar.gz
+# Move wordpress files to the mounted folder
+sudo cp -r wordpress/* /var/www/html/
+# Move to wordpress directory
+cd /var/www/html/
+# Create wp-config file from sample file
+cp wp-config-sample.php wp-config.php
+# Opens the configfile, manually edit
+nano wp-config.php
